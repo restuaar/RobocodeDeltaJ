93083bc39d557b70c6145737040556089ada6f1e
sabrinaatha <sabrinaathashania@gmail.com>
2025-03-14 19:41:49 +0700

    completed tugas 2 week 2

---
+++ RoboRobo/src/jab/module/Targeting.java
@@ -0,0 +1,26 @@
+package jab.module;
+
+import robocode.util.Utils;
+
+/**
+ * Targeting
+ * 
+ * @author jabier.martinez
+ */
+public class Targeting extends Part {
+
+	public Module bot;
+
+	public Targeting(Module bot) {
+		this.bot = bot;
+	}
+
+	public void target() {
+		if (bot.enemy != null) {
+			double absoluteBearing = bot.getHeadingRadians() + bot.enemy.bearingRadians;
+			//System.out.println("HT Bearing: " +bot.enemy.bearingRadians + "   AbsoluteBearing: "+absoluteBearing);
+			bot.setTurnGunRightRadians(Utils.normalRelativeAngle(absoluteBearing - bot.getGunHeadingRadians()));
+		}
+	}
+
+}
